6dc681c195dcd2fac2410bdb81b857bf17ca92aa
Michael Uftring <michael.uftring@gmail.com>
2023-02-17 12:30:01 -0500

    implement: Dataset Proposal table and type definitions

--- terraform/dynamodb.tf
+++ terraform/dynamodb.tf
@@ -53,3 +53,50 @@ resource "aws_dynamodb_table" "repository_questions_dynamo_table" {
     },
   )
 }
+
+resource "aws_dynamodb_table" "dataset_proposals_dynamo_table" {
+  name           = "${var.environment_name}-dataset-proposals-${data.terraform_remote_state.region.outputs.aws_region_shortname}"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "userId"
+  sort_key       = "proposalId"
+
+  attribute {
+    name = "UserId"
+    type = "N"
+  }
+
+  attribute {
+    name = "ProposalId"
+    type = "N"
+  }
+
+  attribute {
+    name = "RepositoryId"
+    type = "N"
+  }
+
+  global_secondary_index {
+    name               = "RepositoryIdIndex"
+    hash_key           = "RepositoryId"
+    range_key          = "UserId"
+    projection_type    = "ALL"
+  }
+
+  point_in_time_recovery {
+    enabled = true
+  }
+
+  server_side_encryption {
+    enabled = true
+  }
+
+  tags = merge(
+    local.common_tags,
+    {
+      "Name"         = "${var.environment_name}-dataset-proposals-${data.terraform_remote_state.region.outputs.aws_region_shortname}"
+      "name"         = "${var.environment_name}-dataset-proposals-${data.terraform_remote_state.region.outputs.aws_region_shortname}"
+      "service_name" = var.service_name
+    },
+  )
+
+}
