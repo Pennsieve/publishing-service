7501e76a0e39d4c7bd948d8d41c0522c8a4e7cec
Michael Uftring <michael.uftring@gmail.com>
2023-02-20 13:35:14 -0500

    fix: DynamoDB table definition range_key

--- terraform/dynamodb.tf
+++ terraform/dynamodb.tf
@@ -58,7 +58,7 @@ resource "aws_dynamodb_table" "dataset_proposals_dynamo_table" {
   name           = "${var.environment_name}-dataset-proposals-${data.terraform_remote_state.region.outputs.aws_region_shortname}"
   billing_mode   = "PAY_PER_REQUEST"
   hash_key       = "UserId"
-  sort_key       = "ProposalId"
+  range_key      = "ProposalId"
 
   attribute {
     name = "UserId"
