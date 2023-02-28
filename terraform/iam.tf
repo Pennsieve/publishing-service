f93487300b31842bc91d53ca7c0e0bb41628362c
Michael Uftring <michael.uftring@gmail.com>
2023-02-28 08:39:17 -0500

    add: PutItem permission

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -66,6 +66,7 @@ data "aws_iam_policy_document" "publishing_service_iam_policy_document" {
       "dynamodb:BatchGetItem",
       "dynamodb:GetItem",
       "dynamodb:PutItem",
+      "dynamodb:DeleteItem",
       "dynamodb:Query",
       "dynamodb:Scan",
       "dynamodb:PartiQLSelect"
