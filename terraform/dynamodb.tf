b3170e6c7df9c17ed779e23514f1145b8fd1a9e3
Michael Uftring <michael.uftring@gmail.com>
2023-03-09 09:58:33 -0500

    change range_key to NodeId on Dataset Proposals

--- terraform/dynamodb.tf
+++ terraform/dynamodb.tf
@@ -58,7 +58,7 @@ resource "aws_dynamodb_table" "dataset_proposals_dynamo_table" {
   name           = "${var.environment_name}-dataset-proposals-${data.terraform_remote_state.region.outputs.aws_region_shortname}"
   billing_mode   = "PAY_PER_REQUEST"
   hash_key       = "UserId"
-  range_key      = "ProposalNodeId"
+  range_key      = "NodeId"
 
   attribute {
     name = "UserId"
@@ -66,7 +66,7 @@ resource "aws_dynamodb_table" "dataset_proposals_dynamo_table" {
   }
 
   attribute {
-    name = "ProposalNodeId"
+    name = "NodeId"
     type = "S"
   }
 
