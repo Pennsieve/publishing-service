32b9fca5fed4fef7efb43c51b9b89a24245c83ab
Michael Uftring <michael.uftring@gmail.com>
2023-05-02 19:10:53 -0400

    add Publishing Info table

--- terraform/dynamodb.tf
+++ terraform/dynamodb.tf
@@ -1,3 +1,26 @@
+resource "aws_dynamodb_table" "publishing_info_dynamo_table" {
+  name           = "${var.environment_name}-publishing-info-${data.terraform_remote_state.region.outputs.aws_region_shortname}"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "Tag"
+
+  point_in_time_recovery {
+    enabled = true
+  }
+
+  server_side_encryption {
+    enabled = true
+  }
+
+  tags = merge(
+    local.common_tags,
+    {
+      "Name"         = "${var.environment_name}-publishing-info-${data.terraform_remote_state.region.outputs.aws_region_shortname}"
+      "name"         = "${var.environment_name}-publishing-info-${data.terraform_remote_state.region.outputs.aws_region_shortname}"
+      "service_name" = var.service_name
+    },
+  )
+}
+
 resource "aws_dynamodb_table" "repositories_dynamo_table" {
   name           = "${var.environment_name}-repositories-${data.terraform_remote_state.region.outputs.aws_region_shortname}"
   billing_mode   = "PAY_PER_REQUEST"
